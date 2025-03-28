32675a953544f555437b46f8b5ddc78d99818c6a
APIVerve Team <hello@apiverve.com>
2025-03-28 10:16:10 -0500

    Bug Fixes

---
+++ SudokuGeneratorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class SudokuGeneratorQueryOptions {
+public string difficulty { get; set; }
+}
+
+}
